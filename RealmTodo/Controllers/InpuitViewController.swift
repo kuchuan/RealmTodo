73fd073ac7c81e96129352f19c95bc6be5c6ad08
堀川浩二 <>
2019-07-30 11:35:31 +0800

    Main.storyboad完成 必要なクラスファイルを作成済み

---
+++ RealmTodo/Controllers/InpuitViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InpuitViewController.swift
+//  RealmTodo
+//
+//  Created by 堀川浩二 on 2019/07/30.
+//  Copyright © 2019 堀川浩二. All rights reserved.
+//
+
+import UIKit
+
+class InpuitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RealmTodo/Objects/Todo.swift
@@ -0,0 +1,9 @@
+//
+//  Todo.swift
+//  RealmTodo
+//
+//  Created by 堀川浩二 on 2019/07/30.
+//  Copyright © 2019 堀川浩二. All rights reserved.
+//
+
+import Foundation
